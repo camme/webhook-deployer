52a034518430d52be61dbaaafbad633117be4754
camilo tapia <camilo.tapia@gmail.com>
2013-04-28 11:54:38 +0200

    trying to make it a command line tool with daemon

--- index.js
+++ index.js
@@ -1,181 +1,34 @@
-var defaultOptions = {
-    port: 8080,
-    config: "deploys.json"
-};
-
-var options = defaultOptions;
-
-var http = require("http");
 var fs = require("fs");
 var path = require("path");
-var querystring = require("querystring");
-var exec  = require('child_process').exec;
-var lastInfo = "";
 
-var packageInformation = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+var packageInformation = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), 'utf8'));
 
-var server = http.createServer(function(req, res) {
+var program = require('commander');
 
+program
+  .version(packageInformation.version)
+  .option('-p, --port [port]', 'Set the port number to use. Defaults to 8080')
+  .option('-c, --configfile [configfile]', 'Set the path to the config file to be used. Default to ./deploys.json')
+  .option('-d, --daemon', 'Run the webhook-deployer as a deamon with forever')
+  .option('-s, --stop', 'Stop webhook-deployer that was run as a deamon')
+  .option('-t, --test', "test")
+  .parse(process.argv);
 
-    var data = "";
-    req.on("data", function(chunk) {
-        data += chunk;
-    });
+var options = {};
 
-    req.on("end", function() {
+console.log("TEST", program.test);
 
-    if (req.method == "POST") {
-
-        req.params = querystring.parse(data);
-
-
-        // this stuf is to handle multipart
-
-        //var boundaryData = /--([\w\d]+)$/.exec(req.headers['content-type']);
-        //var boundary = boundaryData && boundaryData[1];
-
-        //var dataList = data.split(boundary);
-        //dataList.pop();
-
-        //console.log(dataList);
-
-
-        //if (data.match(/content-disposition: form-data;/i)) {
-        //var formDataRe = /content-disposition: form-data;([.\W]*?)/gi;
-        //var formData = formDataRe.exec(data);
-        //console.log(formData);
-        //}
-
-    }
-    else {
-        req.params = {};
-    }
-
-    for(var i = 0, ii = routes.length; i < ii; i++){
-        var route = routes[i];
-        var match  = route.route.exec(req.url);
-        if (match) {
-            req.params.urlData = match && match.length > 0 ? match[1] : null;
-            route.action(req, res); 
-            break;
-        }
-    };
-
-    res.end();
-
-    });
-
-});
-
-function incoming(req, res) {
-
-
-    lastInfo = "Incoming " + req.params.urlData + " - " + (new Date()).toString() + "\n\n";
-
-    var exists = fs.existsSync(options.config);
-
-    if (!exists) {
-        console.error("No config file found at %s", options.config);
-        lastInfo += "NO CONFIG FILE FOUND AT " + path.resolve(__dirname, options.config);
-        res.write(lastInfo);
-        return;
-    }
-    var deployConfig = JSON.parse(fs.readFileSync(options.config, 'utf8'));
-
-
-    deployConfig.deploys.forEach(function(deploy) {
-
-        if (deploy.type == "github") {
-
-            var repoData = JSON.parse(req.params.payload);
-
-            console.log("Checking incoming repo %s ...", repoData.repository.url);
-            lastInfo += "Checking incoming repo " + repoData.repository.url + "...\n";
-
-            if (repoData.repository.url == deploy.repo) {
-
-                var branch = repoData.ref.split("/").pop();
-
-                console.log("  Checking branch '%s' ...", branch);
-                lastInfo += ">  Checking branch '" + branch + "' ...\n";
-                if (branch == deploy.branch) {
-
-                    console.log("\n");
-                    console.log("Run %s with branch '%s'", deploy.name, branch);
-                    console.log("");
-
-                    lastInfo += ">  Run " + deploy.name + " with branch " + branch + "\n";
-                    lastInfo += "\n";
-
-                    var localPath = path.resolve(deploy.basepath);
-
-                    var cmd = exec(deploy.command, {
-                        cwd: localPath
-                    }, function(error, stdout, stderr) {
-                        if (error) {
-                            lastInfo += ">  ERROR (error): " + error + "\n";
-                        }
-                        if (stderr) {
-                            lastInfo += ">  ERROR (stderr): " + stderr + "\n";
-                        }
-                        lastInfo += ">  DONE!";
-                     });
-                    cmd.stdout.on('data', function(data) {
-                        lastInfo += data.toString() + "\n";
-                        process.stdout.write(data.toString());
-                    });
-                    cmd.stdout.on('close', function(data) {
-                        lastInfo += "CLOSE: " + data.toString() + "\n";
-                        process.stdout.write(data.toString());
-                    });
-                    cmd.stdout.on('exit', function(data) {
-                        lastInfo += "EXIT: " + data.toString() + "\n";
-                        process.stdout.write(data.toString());
-                    });
-                     cmd.stdout.on('disconnect', function(data) {
-                        lastInfo += "DISCONNECT: " + data.toString() + "\n";
-                        process.stdout.write(data.toString());
-                    });
-                      cmd.stdout.on('message', function(data) {
-                        lastInfo += "MESSAGE: " + data.toString() + "\n";
-                        process.stdout.write(data.toString());
-                    });
-                    cmd.stderr.on('data', function(data) {
-                        lastInfo += ">  ERROR (on): " + data.toString() + "\n";
-                        process.stdout.write("ERROR " +  data.toString());
-                    });
-
-
-                }
-                else {
-                    console.log("  No, wrong branch, nothing to do");
-                    lastInfo += ">  No, wrong branch, nothing to do\n";
-                }
-            }
-            else {
-                console.log("No, wrong repo, nothing to do");
-                lastInfo += "No, wrong repo, nothing to do\n";
-            }
-
-        }
-    });
-
+if (program.port) {
+    options.port = program.port;
 }
 
-function index(req, res) {
-    res.write("Webhook deployer version " + packageInformation.version + "\n\n");
-    res.write(lastInfo);
-};
-
-var routes = [{
-    route: /^\/incoming\/(.*?)($|\/$)/gi,
-    action: incoming
-},{
-    route: /^\/$/g,
-    action: index
-}]
+if (program.configfile) {
+    options.config = program.configfile;
+}
 
 
-
-
-server.listen(options.port);
+console.log(__dirname);
+//var path = require("path");
+//var webdepPath = path.resolve(__dirname, "../lib
+var webdep = require("./lib/webdep.js");
+webdep.init();
---
+++ test/main.js
@@ -0,0 +1,19 @@
+// need to include should directly here to be able to use not
+//var should = require('should');
+
+describe("No tests yet", function() {
+
+    beforeEach(function(done) {
+        done();
+    });
+
+    afterEach(function(done) {
+        done();
+    });
+
+    //it("makes a get to the google analytics __utm.gif when I fetch ", function(done) {
+        //done();
+    //});
+
+});
+   
